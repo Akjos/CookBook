47220ca393dd52fe1ca691018d5c4651c2b822e4
Akjos <sojka_p@o2.pl>
2020-11-29 16:02:09 +0100

    add Role to db

---
+++ src/main/java/pl/akjos/CookBook/domain/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package pl.akjos.CookBook.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.akjos.CookBook.domain.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
