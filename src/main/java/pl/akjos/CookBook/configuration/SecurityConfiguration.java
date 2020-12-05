9843ecbd28e1744dffeda445618c1f9992035e53
Akjos <sojka_p@o2.pl>
2020-12-05 13:06:22 +0100

    add login page, configure login and log out in SecurityConfiguration

--- src/main/java/pl/akjos/CookBook/configuration/SecurityConfiguration.java
+++ src/main/java/pl/akjos/CookBook/configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -38,9 +39,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .antMatchers("/app/**").authenticated()
+                .antMatchers("/").permitAll()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .and().formLogin();
     }
 }
--- src/main/java/pl/akjos/CookBook/user/UserController.java
+++ src/main/java/pl/akjos/CookBook/user/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
@@ -37,6 +38,11 @@ public class UserController {
         }
         userService.saveUser(userRegisterDTO);
         return "redirect:/login";
+    }
+
+    @RequestMapping("/login")
+    public String loginUser() {
+        return "/login";
     }
 
 }
