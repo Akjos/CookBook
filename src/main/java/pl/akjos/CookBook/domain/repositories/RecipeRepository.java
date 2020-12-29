81a5bb5e61e827e2f114d818e9b5cba05af24c0f
Akjos <sojka_p@o2.pl>
2020-12-29 20:38:00 +0100

    change address recipe dto to correct

--- src/main/java/pl/akjos/CookBook/domain/repositories/RecipeRepository.java
+++ src/main/java/pl/akjos/CookBook/domain/repositories/RecipeRepository.java
@@ -4,13 +4,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.akjos.CookBook.domain.model.Recipe;
-import pl.akjos.CookBook.recipe.RecipeListDTO;
+import pl.akjos.CookBook.recipe.DTO.RecipeListDTO;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-    @Query("SELECT new pl.akjos.CookBook.recipe.RecipeListDTO(r.id, r.name, r.description) FROM Recipe r JOIN r.user u WHERE u.id = :userId")
+    @Query("SELECT new pl.akjos.CookBook.recipe.DTO.RecipeListDTO(r.id, r.name, r.description) FROM Recipe r JOIN r.user u WHERE u.id = :userId")
     List<RecipeListDTO> getRecipesByUserId(Long userId, Pageable pageable);
 
     Integer countByUserId(Long id);
