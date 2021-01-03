f4c5259e9fd16b927b29c1ebcf376f05868479a9
Akjos <sojka_p@o2.pl>
2021-01-03 16:43:14 +0100

    add builder

--- src/main/java/pl/akjos/CookBook/domain/model/Plan.java
+++ src/main/java/pl/akjos/CookBook/domain/model/Plan.java
@@ -2,6 +2,7 @@ package pl.akjos.CookBook.domain.model;
 
 
 import lombok.*;
+import org.hibernate.loader.plan.build.spi.MetamodelDrivenLoadPlanBuilder;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -32,8 +33,46 @@ public class Plan {
     @ManyToOne
     private User user;
 
+    private Plan(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     @PrePersist
     public void prePersist() {
         this.created = LocalDate.now();
+    }
+
+    public static PlanBuilder builder() {
+        return new PlanBuilder();
+    }
+
+    public static class PlanBuilder {
+        private Long id;
+        private String name;
+        private String description;
+
+        PlanBuilder() {
+        }
+
+        public PlanBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public PlanBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlanBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Plan build() {
+            return new Plan(id, name, description);
+        }
     }
 }
